9eabe5c237d44053b1445cd48c2a4f15428a665a
Nick Caminiti <nmcams11@gmail.com>
2023-03-02 16:21:09 -0800

    finish extra credit

--- linkedlist.rb
+++ linkedlist.rb
@@ -6,30 +6,30 @@ class LinkedList
   end
 
   def append(value)
-    if @head == nil
+    if @head.nil?
       @head = Node.new(value)
     else
       current_node = @head
-      current_node = current_node.next_node until current_node.next_node == nil
+      current_node = current_node.next_node until current_node.next_node.nil?
       current_node.next_node = Node.new(value)
     end
   end
 
   def prepend(value)
-    if @head == nil
-      @head = Node.new(value)
-    else
-      @head = Node.new(value, @head)
-    end
+    @head = if @head.nil?
+              Node.new(value)
+            else
+              Node.new(value, @head)
+            end
   end
-  
+
   def size
-    if @head == nil
+    if @head.nil?
       count = 0
     else
       count = 1
       current_node = @head
-      until current_node.next_node == nil
+      until current_node.next_node.nil?
         current_node = current_node.next_node
         count += 1
       end
@@ -38,7 +38,7 @@ class LinkedList
   end
 
   def tail
-    if head == nil
+    if head.nil?
       nil
     else
       current_node = @head
@@ -46,9 +46,9 @@ class LinkedList
       current_node
     end
   end
-  
+
   def pop
-    if head == nil
+    if head.nil?
       nil
     else
       current_node = @head
@@ -58,14 +58,14 @@ class LinkedList
   end
 
   def at(index)
-    if @head == nil
+    if @head.nil?
       nil
     else
       count = 0
       current_node = @head
       until count == index
         current_node = current_node.next_node 
-        count +=1
+        count += 1
       end
       current_node
     end
@@ -77,35 +77,68 @@ class LinkedList
     until current_node.next_node == nil
       contains = true if current_node.value == value
       break if contains
+
       current_node = current_node.next_node
     end
     contains
   end
 
   def find(value)
-    if self.contains(value)
+    if contains(value)
       index = 0
       current_node = @head  
-      until current_node.next_node == nil
+      until current_node.next_node.nil?
         break if current_node.value == value
+
         current_node = current_node.next_node
         index += 1
       end
       index
-    else
-      nil
     end
   end
 
   def to_s
     string = ''
     current_node = @head
-    until current_node.next_node == nil
-      string += "( #{current_node.value.to_s} ) -> "
-      current_node = current_node.next_node
+    unless @head.next_node.nil?
+      until current_node.next_node == nil
+        string += "( #{current_node.value.to_s} ) -> "
+        current_node = current_node.next_node
+      end
     end
     string += "( #{current_node.value.to_s} ) -> nil"
     string
+  end
+
+  def insert_at(value, index)
+    if index.zero?
+      prepend(value)
+    else
+      node_num = 1
+      current_node = @head
+      until node_num == index
+        current_node = current_node.next_node
+        node_num += 1
+      end
+      puts current_node
+      new_next_node = current_node.next_node
+      current_node.next_node = Node.new(value, new_next_node)
+    end
+  end
+
+  def remove_at(index)
+    node_num = 0
+    current_node = @head
+    if index.zero?
+      @head = @head.next_node
+    else
+      until node_num == index
+        prior_node = current_node
+        current_node = current_node.next_node
+        node_num += 1
+      end
+      prior_node.next_node = current_node.next_node
+    end
   end
 end
 
@@ -145,3 +178,9 @@ p list.contains('number 4')
 p list.find('number 1')
 p list.find('number 4')
 p list.to_s
+list.insert_at('number 1.5', 1)
+p list
+p list.size
+list.remove_at(3)
+p list
+p list.size
